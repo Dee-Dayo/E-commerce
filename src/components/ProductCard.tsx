ad5698fd3e4bb054a6ec0b77215e81a6a1e3a9a6
Dayo Akinyemi <darhyor2050@gmail.com>
2024-11-12 17:14:41 +0100

    finished shop page

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,34 +1,46 @@
-import {ProductData} from "../../type";
+import { ProductData } from "../../type";
 import Link from "next/link";
 import Image from "next/image";
-import {urlFor} from "@/sanity/lib/image";
-import {MdStar} from "react-icons/md";
+import { urlFor } from "@/sanity/lib/image";
+import { MdStar } from "react-icons/md";
+import FormattedPrice from "@/components/FormattedPrice";
+import AddToCartButton from "@/components/AddToCartButton";
 
-const ProductCard = ({item}: {item: ProductData}) => {
+const ProductCard = ({ item }: { item: ProductData }) => {
     return (
         <div className='border border-px border-lightText/40 rounded-md relative group overflow-hidden'>
-           <div className='overflow-hidden'>
+            <div className='overflow-hidden'>
                 <Link href={`/product/${item?.slug.current}`}>
-                <Image
-                    src={urlFor(item?.image).url()}
-                    alt={item?._type}
-                    width={500}
-                    height={500}
-                    className='w-full h-72 object-contain group-hover:scale-105 hoverEffect'
-                />
-            </Link>
-           </div>
+                    <Image
+                        src={urlFor(item?.image).url()}
+                        alt={item?._type}
+                        width={500}
+                        height={500}
+                        className='w-full h-72 object-contain group-hover:scale-105 hoverEffect'
+                    />
+                </Link>
+            </div>
             <div className='px-6 flex flex-col items-center gap-2'>
                 <div className='text-base text-lightText flex items-center'>
-                    {Array?.from({length:5})?.map((_,index)=>{
-                        const filled = index +1 <= Math.floor(item?.ratings);
-                        const halfFilled = index +1 > Math.floor(item?.ratings) && index < Math.ceil(item?.ratings);
+                    {Array?.from({ length: 5 })?.map((_, index) => {
+                        const filled = index + 1 <= Math.floor(item?.ratings);
+                        const halfFilled = index + 1 > Math.floor(item?.ratings) && index < Math.ceil(item?.ratings);
                         return (
-                            <MdStar className={`${filled ? "text-[#fa8900]":halfFilled ? "text-yellow-300" : "text-lightText"}`} key={index}/>
+                            <MdStar className={`${filled ? "text-[#fa8900]" : halfFilled ? "text-yellow-300" : "text-lightText"}`} key={index} />
                         )
                     })}
                 </div>
+                <div className='text-center'>
+                    <p className='uppercase text-sx font-medium text-lightOrange'>{item?.brand}</p>
+                    <h2 className='text-base font-semibold text-accent line-clamp-1'>{item?.title}</h2>
+                    <p className='text-sm line-clamp-2'>{item?.description}</p>
+                    <div className='flex items-center justify-center gap-3 mb-5'>
+                        <FormattedPrice amount={item?.rowprice} className='text-lightText line-through' />
+                        <FormattedPrice amount={item?.price} className='text-darkOrange font-bold' />
+                    </div>
+                </div>
             </div>
+            <AddToCartButton item={item}/>
         </div>
     )
 }
