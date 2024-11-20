31974b41a42a5b30132d2d7e591fc8a28962a18c
Dayo Akinyemi <darhyor2050@gmail.com>
2024-11-20 16:40:37 +0100

    finished storing into local storage for carts

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,16 @@
 'use client'
 
 import {Provider} from "react-redux";
-import {store} from "@/redux/store";
+import {persistor, store} from "@/redux/store";
+import {PersistGate} from "redux-persist/integration/react";
 
 const Layout = ({children}:{children:React.ReactNode}) => {
     return (
-        <Provider store={store}>{children}</Provider>
+        <Provider store={store}>
+            <PersistGate persistor={persistor}>
+                {children}
+            </PersistGate>
+        </Provider>
     )
 }
 
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,59 @@
 import { configureStore } from "@reduxjs/toolkit";
 import jumiaReducer from "@/redux/jumiaSlice";
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    REGISTER,
+    WebStorage,
+} from 'redux-persist';
+import storage from "redux-persist/lib/storage";
+import {PURGE} from "redux-persist/es/constants";
+import createWebStorage from "redux-persist/es/storage/createWebStorage";
+
+export function createPersistStorage():WebStorage {
+    const isServer = typeof window === "undefined";
+
+    if(isServer){
+        return {
+            getItem(){
+                return Promise.resolve(null);
+            },
+            setItem(){
+                return Promise.resolve();
+            },
+            removeItem(){
+                return Promise.resolve();
+            },
+        };
+    }
+    return createWebStorage("local");
+};
+
+const customStorage=typeof window !== 'undefined'
+    ? createWebStorage("local")
+    : createPersistStorage();
+
+const persistConfig = {
+    key: "root",
+    storage: customStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, jumiaReducer)
 
 export const store = configureStore({
     reducer: {
-        jumia: jumiaReducer,
+        jumia: persistedReducer,
     },
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+    }),
 });
+
+export const persistor = persistStore(store);
