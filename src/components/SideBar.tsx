b3358d27511c2772eb6d430423be0e4a19f99e38
Dayo Akinyemi <darhyor2050@gmail.com>
2024-12-09 16:56:19 +0100

    finished firebase configuration

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,8 +1,12 @@
 import Link from "next/link";
 import {MdSwitchAccount} from "react-icons/md";
 import SideBarCart from "@/components/SideBarCart";
+import {auth} from "@/auth";
+import Image from 'next/image';
 
-const SideBar = () => {
+const SideBar = async() => {
+    const session= await auth();
+
     return (
         <div className='fixed top-60 right-2 z-20 flex flex-col gap-2'>
             {/*User account*/}
@@ -12,8 +16,17 @@ const SideBar = () => {
                 gap-1 text-accent justify-center items-center shadow-sm
                 shadow-lightGreen group overflow-hidden'>
                 <div className='flex items-center justify-center'>
-                    <MdSwitchAccount className='text-2xl -translate-x-12
-                    group-hover:translate-x-3 transition-transform duration-200'/>
+                    {session ?.user ? (
+                        <Image
+                            src={session?.user?.image as string}
+                            alt='userImage'
+                            width={35}
+                            height={35}
+                        />
+                        ) : (
+                            <MdSwitchAccount className='text-2xl -translate-x-12
+                            group-hover:translate-x-3 transition-transform duration-200'/>
+                        )}
                     <MdSwitchAccount className='text-2xl -translate-x-3
                     group-hover:translate-x-12 transition-transform duration-200'/>
                 </div>
